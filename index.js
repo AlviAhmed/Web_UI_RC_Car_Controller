19e59a84f3e92def0eba7e45452ceb88b46a7a2e
AlviAhmed <alviahmed1234@gmail.com>
2020-12-08 15:10:09 -0500

    continous fire of keyboard and updating now working

--- index.js
+++ index.js
@@ -2,17 +2,26 @@
 var numUserButtons = document.querySelectorAll(".keyBut").length; 
 var numUserButtons2 = document.querySelectorAll(".keyBut2").length; 
 
+
+document.addEventListener("keydown", alertKey);
+function alertKey(event){
+    cmdKeys(event.key);
+    updateFunc();
+}
+
+document.addEventListener("keyup", setNeutral);
 var serNum = "Receiver #1";
 var cmd = "Neutral";
 updateFunc();
 
-document.querySelector("body").addEventListener("click",updateFunc);
+
+document.addEventListener("click",updateFunc);
 for (var i = 0; i < numUserButtons; i ++){
-    document.querySelectorAll(".keyBut")[i].addEventListener("click",cLick);
+    document.querySelectorAll(".keyBut")[i].addEventListener("click",cmdClick);
     
-}
+} 
 for (var i = 0; i < numUserButtons2; i ++){
-    document.querySelectorAll(".keyBut2")[i].addEventListener("click",cLick2);
+    document.querySelectorAll(".keyBut2")[i].addEventListener("click",rxClick);
 }
 
 function updateFunc(){
@@ -20,9 +29,11 @@ console.log("Command: " + cmd + " RX: " + serNum);
 document.querySelector(".output").innerHTML = "Command: " + cmd + " RX: " + serNum;
 }
 
-function cLick(){
-    var buttonName=this.innerHTML;
-    this.classList.add("pressed");
+function setNeutral(){
+    cmd = "Neutral";
+    updateFunc();
+}
+function cmdKeys(buttonName){
     switch (buttonName){
     case "w":
         cmd = "Forward";
@@ -36,12 +47,34 @@ function cLick(){
     case "d":
         cmd = "Right";
         break;
+    case "1":
+        serNum = "Receiver #1";
+        break;
+    case "2":
+        serNum =  "Receiver #2";
+        break;
     default:
         cmd = "Neutral";
     }
 }
 
-function cLick2(){
+// function rxKeys(buttonName){
+//     switch (buttonName){
+//     default:
+//         serNum = "Receiver #1";
+//     }
+// }
+
+function cmdClick(event){
+    console.log(event);
+    var buttonName=this.innerHTML;
+    this.classList.add("pressed");
+    cmdKeys(buttonName);
+}
+
+
+
+function rxClick(){
     // alert("I got clicked");
     // console.log(this.innerHTML);
     var buttonName=this.innerHTML;
@@ -57,6 +90,7 @@ function cLick2(){
         serNum = "Receiver #1";
     }
 }
+
 
 
 
